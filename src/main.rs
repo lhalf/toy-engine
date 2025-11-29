621fcdfbfc60da639cd6f387786ae21d84950a90
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-29 21:38:39 +0000

    add initial csv reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Context;
+
+const BUFFER_CAPACITY: usize = 64 * 1024;
+
+fn main() -> anyhow::Result<()> {
+    let path = std::env::args().next().context("missing argument")?;
+
+    let _reader = csv::ReaderBuilder::new()
+        .buffer_capacity(BUFFER_CAPACITY)
+        .trim(csv::Trim::All)
+        .from_path(&path)?;
+
+    Ok(())
 }
