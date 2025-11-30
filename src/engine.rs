217ff8db414b7daa0b0653226ae87ab037b72c2d
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-30 16:49:33 +0000

    dispute non existent transaction does nothing

--- src/engine.rs
+++ src/engine.rs
@@ -174,4 +174,19 @@ mod tests {
         assert_eq!(1, engine.accounts.len());
         assert_eq!((0.0, 1.0), engine.available_and_held_for_client(1));
     }
+
+    #[test]
+    fn disputing_a_non_existent_transaction_does_nothing() {
+        let mut engine = Engine::default();
+
+        engine.handle_transaction(Transaction::deposit(1, 1, 1.0));
+
+        assert_eq!(1, engine.accounts.len());
+        assert_eq!((1.0, 0.0), engine.available_and_held_for_client(1));
+
+        engine.handle_transaction(Transaction::dispute(1, 2));
+
+        assert_eq!(1, engine.accounts.len());
+        assert_eq!((1.0, 0.0), engine.available_and_held_for_client(1));
+    }
 }
