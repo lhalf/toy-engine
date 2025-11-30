63b31c84f472a8a6bf1a96a4820cc83b2879f707
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-30 19:25:15 +0000

    add test for missing argument

--- tests/integration.rs
+++ tests/integration.rs
@@ -4,13 +4,24 @@ fn call_toy_engine(args: &[&str]) -> Output {
     Command::new("./target/release/toy-engine")
         .args(args)
         .stdout(Stdio::piped())
-        .stderr(Stdio::null())
+        .stderr(Stdio::piped())
         .output()
         .unwrap()
 }
 
 #[test]
-fn handles_invalid_orders() {
+fn missing_argument() {
+    let output = call_toy_engine(&[]);
+
+    assert!(!output.status.success());
+    assert_eq!(
+        "Error: missing argument\n",
+        String::from_utf8_lossy(output.stderr.as_slice())
+    );
+}
+
+#[test]
+fn ignores_invalid_orders() {
     let output = call_toy_engine(&["tests/data/some_invalid.csv"]);
 
     assert!(output.status.success());
